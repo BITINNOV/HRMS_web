a8c146abf1e49b9acda4602311eb709be97e9e25
Soufian EL MORABIT <selmorabit@bitinnov.ma>
2022-12-23 23:41:40 +0100

    Update 23/12/2022

---
+++ src/app/shared/models/configuration/front-end/view-language.spec.ts
@@ -0,0 +1,7 @@
+import { ViewLanguage } from './view-language';
+
+describe('ViewLanguage', () => {
+  it('should create an instance', () => {
+    expect(new ViewLanguage()).toBeTruthy();
+  });
+});
---
+++ src/app/shared/models/configuration/front-end/view-language.ts
@@ -0,0 +1,4 @@
+export class ViewLanguage {
+  code: String;
+  name: String;
+}
